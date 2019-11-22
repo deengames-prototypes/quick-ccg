302edb384fde777e412352487f14d7e52be980ed
nightblade9 <nightbladecodes@gmail.com>
2019-11-21 19:57:34 -0500

    Universe/card generation

---
+++ prototypes/SimplerRps/Globals.gd
@@ -0,0 +1,18 @@
+extends Node
+
+const MAX_VALUE = 6
+const UNIVERSE_SIZE = 50 # 50 cards in all
+var TYPES = ["Triangle", "Circle", "Square"]
+
+var all_cards = []
+
+func _ready():
+	for i in range(UNIVERSE_SIZE):
+		# Dupes are OK. Don't really care.
+		# [1..max]
+		var strength = (randi() % MAX_VALUE) + 1
+		var defense = (randi() % MAX_VALUE) + 1
+		var type = TYPES[randi() % len(TYPES)]
+		var card = {"strength": strength, "defense": defense, "type": type}
+		all_cards.append(card)
+		print(str(card))
