11cc982ea7d30b0d859526b3a7b19974de00938d
nightblade9 <nightbladecodes@gmail.com>
2019-11-21 19:44:27 -0500

    Board tile and board

--- prototypes/SimplerRps/Card.gd
+++ prototypes/SimplerRps/Card.gd
@@ -4,7 +4,6 @@ export(String) var affinity = "Triangle"
 export(int) var strength = 1
 export(int) var defense = 1
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Label.text = str(strength) + "/" + str(defense)
 	$Sprite.texture = load("res://" + affinity.to_lower() + ".png")
