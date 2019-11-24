70724cec759886aa2d9ebf96a961927c21f82254
nightblade9 <nightbladecodes@gmail.com>
2019-11-24 16:20:47 -0500

    Difficulty tweak: 20% chance of random non-optimal move

--- prototypes/3-OneNumber/CoreGameScene.gd
+++ prototypes/3-OneNumber/CoreGameScene.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const RANDOM_MOVE_PROBABILITY = 0.25
+
 var _player_card
 
 func _ready():
@@ -28,26 +30,33 @@ func _on_player_tile_click(tile):
 # drawing out opponent, saving best for last, etc.)
 func _ai_do_something():
 	### rare crash: we have no cards
-	if len($AiDeck.tiles) > 0:
+	if len($AiDeck.tiles) > 0:		
 		var card_tile = $AiDeck.tiles[randi() % len($AiDeck.tiles)]
-		
 		var best = null
-		var best_score = -1 # score of zero is possible, still should pick it if it's the best move
 		
-		for tile in $Board.tiles:
-			if tile.occupant == null:
-				# empty tile, look at adjacencies to calculate number of wins
-				var adjacencies = $Board.get_adjacencies(tile)
-				var total_score = 0
-	
-				for adjacent in adjacencies:
-					var target = adjacent.occupant
-					if target != null and target.owned_by != card_tile.owned_by:
-						total_score += Globals.calculate_damage(card_tile, adjacent.occupant)
-				
-				if total_score > best_score:
-					best_score = total_score
-					best = tile
+		# x% chance of placing on a random tile
+		if randf() <= RANDOM_MOVE_PROBABILITY:
+			best = $Board.tiles[randi() % len($Board.tiles)]
+			while best.occupant != null:
+				best = $Board.tiles[randi() % len($Board.tiles)]
+		else:
+			# Random card, place best move
+			var best_score = -1 # score of zero is possible, still should pick it if it's the best move
+			
+			for tile in $Board.tiles:
+				if tile.occupant == null:
+					# empty tile, look at adjacencies to calculate number of wins
+					var adjacencies = $Board.get_adjacencies(tile)
+					var total_score = 0
+		
+					for adjacent in adjacencies:
+						var target = adjacent.occupant
+						if target != null and target.owned_by != card_tile.owned_by:
+							total_score += Globals.calculate_damage(card_tile, adjacent.occupant)
+					
+					if total_score > best_score:
+						best_score = total_score
+						best = tile
 		
 		$AiDeck.remove_card(card_tile)
 		best.set_occupant(card_tile)
